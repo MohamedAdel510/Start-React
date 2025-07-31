9ea3f3394ebfa62831491535acbf602b66ad1e20
MohamedAdel510 <119543542+MohamedAdel510@users.noreply.github.com>
2025-07-31 03:02:32 +0300

    nav-scroll

--- src/assets/Compondnts/navebar/Navbar.jsx
+++ src/assets/Compondnts/navebar/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect , useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
-// import NavbarStyle from "./Navbar.module.css";
 import './navbar.css';
 export default function Navbar() {
+
+  const [scrolled, setscrolled] = useState(false);
+
+  function handleScroll(){
+    setscrolled(window.scrollY > 50);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
+  
+  
   return (
     <>
-        <nav className= "navbar navbar-expand-lg navbar-dark  fixed-top  py-4">
+        <nav className={"navbar navbar-expand-lg navbar-dark  fixed-top " + (scrolled && "py-4" )}>
           <div className="container">
             <Link id='mohamed' className="navbar-brand" to="/home">Start Framework</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
